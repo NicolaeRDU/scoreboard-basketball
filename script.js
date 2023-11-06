40e9c1d6ab24ec126a2077329d5d8aaefda842e8
Radu <mihairadu8039@gmail.com>
2023-11-06 20:37:24 +0200

    Add a reset button, change the style of the page and made de js code more readable

--- script.js
+++ script.js
@@ -1,79 +1,99 @@
 "use strict";
 
+const team1 = document.querySelector("#team--1");
+const team2 = document.querySelector("#team--2");
+
 const teamScore1 = document.querySelector(".team__score--1");
 const teamScore2 = document.querySelector(".team__score--2");
 
-const plusHosts = document.querySelector(".hosts--plus__one");
-const plusTwoHosts = document.querySelector(".hosts--plus__two");
-const plusThreeHosts = document.querySelector(".hosts--plus__three");
-const plusGuests = document.querySelector(".guests--plus__one");
-const plusTwoGuests = document.querySelector(".guests--plus__two");
-const plusThreeGuests = document.querySelector(".guests--plus__three");
-
+const buttonsAddScore = document.querySelectorAll(".btn");
 const btnDeleteHosts = document.querySelector(".hosts--delete");
 const btnDeleteGuests = document.querySelector(".guests--delete");
+const btnReset = document.querySelector(".btn--reset");
 
-const winnerShow = document.querySelector(".winner");
+const winnerShow = document.querySelector(".winnerAnnouncement");
 const btnWinner = document.querySelector(".btn--winner");
 
 let playing = true;
 
-let sum1 = 0;
-let sum2 = 0;
+let sumHosts = 0;
+let sumGuests = 0;
 
-const init = function () {
+buttonsAddScore.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    if (playing) {
+      if (btn.classList.contains("hosts--plus-one")) {
+        sumHosts += 1;
+        teamScore1.textContent = sumHosts;
+      } else if (btn.classList.contains("guests--plus-one")) {
+        sumGuests += 1;
+        teamScore2.textContent = sumGuests;
+      }
+
+      if (btn.classList.contains("hosts--plus-two")) {
+        sumHosts += 2;
+        teamScore1.textContent = sumHosts;
+      } else if (btn.classList.contains("guests--plus-two")) {
+        sumGuests += 2;
+        teamScore2.textContent = sumGuests;
+      }
+
+      if (btn.classList.contains("hosts--plus-three")) {
+        sumHosts += 3;
+        teamScore1.textContent = sumHosts;
+      } else if (btn.classList.contains("guests--plus-three")) {
+        sumGuests += 3;
+        teamScore2.textContent = sumGuests;
+      }
+    }
+  })
+);
+
+btnDeleteHosts.addEventListener("click", function () {
   if (playing) {
-    plusHosts.addEventListener("click", function () {
-      sum1 += 1;
-      teamScore1.textContent = sum1;
-    });
-
-    plusTwoHosts.addEventListener("click", function () {
-      sum1 += 2;
-      teamScore1.textContent = sum1;
-    });
-
-    plusThreeHosts.addEventListener("click", function () {
-      sum1 += 3;
-      teamScore1.textContent = sum1;
-    });
-
-    btnDeleteHosts.addEventListener("click", function () {
-      sum1 = 0;
-      teamScore1.textContent = sum1;
-    });
-
-    plusGuests.addEventListener("click", function () {
-      sum2 += 1;
-      teamScore2.textContent = sum2;
-    });
-
-    plusTwoGuests.addEventListener("click", function () {
-      sum2 += 2;
-      teamScore2.textContent = sum2;
-    });
-
-    plusThreeGuests.addEventListener("click", function () {
-      sum2 += 3;
-      teamScore2.textContent = sum2;
-    });
-
-    btnDeleteGuests.addEventListener("click", function () {
-      sum2 = 0;
-      teamScore2.textContent = sum2;
-    });
+    sumHosts = 0;
+    teamScore1.textContent = sumHosts;
   }
-};
+});
 
-init();
+btnDeleteGuests.addEventListener("click", function () {
+  if (playing) {
+    sumGuests = 0;
+    teamScore2.textContent = sumGuests;
+  }
+});
 
 btnWinner.addEventListener("click", function () {
   playing = false;
-  if (sum1 > sum2) {
+  if (sumHosts > sumGuests) {
     winnerShow.textContent = `Hosts have won`;
-  } else if (sum1 < sum2) {
+    team1.classList.add("winner");
+    team2.classList.add("losser");
+  } else if (sumHosts < sumGuests) {
     winnerShow.textContent = `Guests have won`;
-  } else winnerShow.textContent = `Draw`;
-  sum1 = sum2 = 0;
+    team2.classList.add("winner");
+    team1.classList.add("losser");
+  } else {
+    winnerShow.textContent = `Draw`;
+    team1.classList.add("draw");
+    team2.classList.add("draw");
+  }
+});
+
+btnReset.addEventListener("click", function () {
+  playing = true;
+
+  sumHosts = 0;
+  sumGuests = 0;
+
   teamScore1.textContent = teamScore2.textContent = 0;
+
+  team1.classList.remove("draw");
+  team2.classList.remove("draw");
+  team1.classList.remove("winner");
+  team2.classList.remove("winner");
+  team1.classList.remove("losser");
+  team2.classList.remove("losser");
+
+  winnerShow.textContent = "";
 });
